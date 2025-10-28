614516820c67b67ddbbf9421493b4507c6b29fbb
itc-ss24007 <s24007@std.it-college.ac.jp>
2025-10-28 10:38:06 +0900

    ニュースにSSRを適用

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -11,6 +11,8 @@ type Props = {
   searchParams: { dk?: string };
 };
 
+export const revalidate = 0;
+
 export default async function Page({ params, searchParams }: Props) {
   const data = await getNewsDetail(params.slug, {
     draftKey: searchParams.dk,
