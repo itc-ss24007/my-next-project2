76529de7aaae56001704321e7408cb3e158fe62e
itc-ss24007 <s24007@std.it-college.ac.jp>
2025-10-28 10:57:50 +0900

    ニュースにISRを適用

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -11,7 +11,7 @@ type Props = {
   searchParams: { dk?: string };
 };
 
-export const revalidate = 0;
+export const revalidate = 60;
 
 export default async function Page({ params, searchParams }: Props) {
   const data = await getNewsDetail(params.slug, {
